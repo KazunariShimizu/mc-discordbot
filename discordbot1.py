4734abc409c601b3da8ef9f5aa1c2e06890d6a66
KazunariShimizu <shimizu@tora-v.net>
2018-11-12 05:05:10 -0800

    statusの追加

--- discordbot1.py
+++ discordbot1.py
@@ -4,6 +4,7 @@
 import os
 import discord # インストールした discord.py
 
+
 client = discord.Client() # 接続に使用するオブジェクト
 
 # 起動時に通知してくれる処理
@@ -15,7 +16,12 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('/neko'):
+        
         reply = 'にゃーん'
+        MinecraftServer("mc.toraden.com", 25565)
+        server = MinecraftServer.lookup("mc.toraden.com:25565")
+        status = server.status()
+        reply = "The server has {0} players and replied in {1} ms".format(status.players.online, status.latency)
         await client.send_message(message.channel, reply)
 
 # botの接続と起動
