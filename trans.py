49b980a64b3c7f9fe95f817791c0ecc133395b3a
Junki Kizuka <k.falconws@gmail.com>
2018-11-13 17:17:01 +0900

    implements en_to_ja, ja_to_en translation

---
+++ trans.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from langdetect import detect
+from py_translator import Translator
+
+
+class Trans:
+
+    def __init__(self, txt):
+        self.txt = txt
+        self.translated = None
+        self.translator = Translator()
+
+    def en_to_ja(self):
+        self.translated = self.translator.translate(
+            text=self.txt, dest="ja").text
+
+    def ja_to_en(self):
+        self.translated = self.translator.translate(
+            text=self.txt, dest="en").text
+
+    def translate(self):
+        lang = detect(self.txt)
+
+        if "en" == lang:
+            self.en_to_ja()
+        elif "ja" == lang:
+            self.ja_to_en()
+        else:
+            print("cannot detect lang")
+            return "cannot detect lang"
+
+        return self.translated
+
+
+if __name__ == "__main__":
+    txt = "Hello world"
+    t = Trans(txt)
+    print(t.translate())
+
+    txt = "こんにちは世界"
+    t = Trans(txt)
+    print(t.translate())
