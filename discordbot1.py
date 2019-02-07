c275cff500c202326b8e6d8fefaeebe52f0ff61c
Junki Kizuka <k.falconws@gmail.com>
2019-02-08 01:05:57 +0900

    refactoring: raw strings etc..

--- discordbot1.py
+++ discordbot1.py
@@ -16,6 +16,8 @@ import joke
 from mcstatus import MinecraftServer
 
 LOG_FILE = os.path.join(os.path.dirname(__file__), "discord.log")
+DOMAIN = "toraden.aa0.netvolante.jp"
+PORT = 25565
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.WARNING)
@@ -28,6 +30,7 @@ TRANSLATE_CMD = CMD_PREFIX + "t "
 JOKE_CMD = CMD_PREFIX + "g "
 NEKO_CMD = CMD_PREFIX + "neko"
 
+
 def _extract_minecraft_chat(chat):
     """マイクラチャットから入力値\t のみを抽出する
     player » \t english translation test (￥t 園gl位sh tr案sl阿智音 手st)
@@ -35,16 +38,17 @@ def _extract_minecraft_chat(chat):
 
     if re.search(r"\(.*\)", chat):
         # LunaChat is on
-        match = re.search("»\s\\\\t\s(.*)\s\(", chat)
+        match = re.search(r"»\s\\t\s(.*)\s\(", chat)
         if match:
             return match.group(1)
     else:
         # LunaChat is off
-        match = re.search("»\s\\\\t\s(.*)", chat)
+        match = re.search(r"»\s\\t\s(.*)", chat)
         if match:
             return match.group(1)
 
     return None
+
 
 class MyClient(discord.Client):
     """discord bot class"""
@@ -64,13 +68,13 @@ class MyClient(discord.Client):
         minecraft_chat = _extract_minecraft_chat(message.content)
 
         if message.content.startswith(NEKO_CMD) \
-             or re.search("\\\\neko", message.content):
+             or re.search(r"\\neko", message.content):
             reply = 'にゃーん '
-            server = MinecraftServer.lookup("toraden.aa0.netvolante.jp:25565")
+            server = MinecraftServer.lookup(DOMAIN + ":" + str(PORT))
             status = server.status()
-            reply += "toraden.aa0.netvolante.jpのプレーヤーは {0} players 居るよ！"\
-                     "速さは、replied in {1} ms だよ！"\
-                     .format(status.players.online, status.latency)
+            reply += DOMAIN + "のプレーヤーは {0} players 居るよ！"\
+                "速さは、replied in {1} ms だよ！"\
+                .format(status.players.online, status.latency)
             await client.send_message(message.channel, reply)
         elif message.content.startswith(TRANSLATE_CMD):
             # 翻訳してみる
@@ -81,10 +85,11 @@ class MyClient(discord.Client):
             t = trans.Trans(minecraft_chat)
             await client.send_message(message.channel, t.translate())
         elif message.content.startswith(JOKE_CMD) \
-             or re.search("»\s\\\\g\s", message.content):
+             or re.search(r"»\s\\g\s", message.content):
             # ジョークレスポンス
             j = joke.Joke()
             await client.send_message(message.channel, j.choice())
+
 
 if __name__ == "__main__":
     # botの接続と起動
